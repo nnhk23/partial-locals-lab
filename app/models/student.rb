7070101e4da3855004c86455858ecddf44a27799
Kim Nguyen <nnhk23@gmail.com>
2020-11-04 15:40:37 -0800

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    if !query.empty?
+      Student.where("name like ?", "%#{query}%").to_a
+    else
+      Student.all
+    end
+  end
+
+
 end
